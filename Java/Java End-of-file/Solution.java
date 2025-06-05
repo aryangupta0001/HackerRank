9d084797e73d4eb7af08cd1af7271668d46e1478
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-06-06 01:56:13 +0530

    Create Solution.java

---
+++ Java/Java End-of-file/Solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner Sc = new Scanner(System.in);
+        int i = 0;
+        
+        while(Sc.hasNext()){
+            i++;
+            String a = Sc.nextLine();
+            System.out.println(i + " " + a);
+        }
+    }
+}
